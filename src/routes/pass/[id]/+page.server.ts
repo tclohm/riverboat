954681ebe74f9fee908cd61c6561028ef9931afd
Taylor Lohman <taylorlohman@duck.com>
2025-11-05 19:09:31 -0800

    page.server id update for dbs

--- src/routes/pass/[id]/+page.server.ts
+++ src/routes/pass/[id]/+page.server.ts
@@ -1,9 +1,11 @@
-import { db, passes } from '$lib/db';
-import { eq } from 'drizzle-orm';
+import { passes } from '$lib/db/schema';
+import { getDB } from '$lib/db';
 import { error } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
 
-export async function load({ params }) {
-  const pass = db.select().from(passes).where(eq(passes.id, parseInt(params.id))).get();
+export async function load({ params, platform }) {
+  const db = getDB(platform);
+  const pass = await db.select().from(passes).where(eq(passes.id, parseInt(params.id))).get();
 
   if (!pass) {
     throw error(404, 'Pass not found');
