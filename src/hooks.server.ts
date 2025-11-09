29d1a595175d95c952a03683b18246a03aceed65
Taylor Lohman <taylorlohman@duck.com>
2025-11-09 13:02:31 -0800

    database init on server start

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,8 @@
-import { getSessionUser } from "$lib/server/auth";
+import { initializeDb } from '$lib/db';
 import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const token = event.cookies.get('session');
-  
-  if (token) {
-    const user = await getSessionUser(event.platform, token);
-    event.locals.user = user;
-  } else {
-    event.locals.user = null;
-  }
-
+  // connection on server start
+  await initializeDb(event.platform);
   return resolve(event);
 };
