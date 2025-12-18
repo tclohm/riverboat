91608c391efae227770a92fd4feac90e2ae5edce
Taylor Lohman <taylorlohman@duck.com>
2025-12-18 15:44:21 -0800

    import path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
