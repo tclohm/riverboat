959e8575aea19543e62b070985a5a9b81cdf4581
Taylor Lohman <taylorlohman@duck.com>
2025-12-19 08:20:04 -0800

    mock db

---
+++ src/lib/db/mock.ts
@@ -0,0 +1,24 @@
+export const mockUsers = [
+  {
+    id: 'user-1',
+    name: 'Test User',
+    email: 'test@example.com',
+    emailVerified: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+];
+
+export const mockPasses = [
+  {
+    id: 1,
+    title: 'Dream Key',
+    price: 110,
+    passType: 'Dream Key',
+    bookedDates: '[]',
+    userId: 'user-1',
+  }
+];
+
+export const mockInquiries = [];
+export const mockNotifications = [];
