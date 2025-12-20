ef73c6edd06aa745dddf7461744f12bbe7f885e7
Taylor Lohman <taylorlohman@duck.com>
2025-12-19 20:39:49 -0800

    e2e passing

--- src/tests/e2e/auth.e2e.test.ts
+++ src/tests/e2e/auth.e2e.test.ts
@@ -2,13 +2,13 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Auth E2E', () => {
   test('should load login page', async ({ page }) => {
-    await page.togo('http://localhost:5173/login');
-    await expect(page.locator('h1').toContainText('Login'));
+    await page.goto('http://localhost:5173/login');
+    await expect(page).toHaveTitle(/Login/);
   });
 
   test('should load signup page', async ({ page }) => {
-    await page.togo('http://localhost:5173/signup');
-    await expect(page.locator('h1').toContainText('Create Account'));
+    await page.goto('http://localhost:5173/signup');
+    await expect(page).toHaveTitle(/Create Account/);
   })
 
   test('should load home page', async ({ page }) => {
